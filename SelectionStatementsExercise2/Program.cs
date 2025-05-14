8b5d07040f9b3b92733c0de92759625055062e07
AlexTrejo <alext8005@yahoo.com>
2025-05-13 23:09:34 -0600

    finished exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            string favoriteSubject = Console.ReadLine();
+
+            switch (favoriteSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("The infinite in mathematics is always unruly, unless is properly trated");
+                    break;
+                
+                case "science":
+                    Console.WriteLine("Science is organized knowledge. Wisdom is organized life.");
+                    break;
+                
+                case "history":
+                    Console.WriteLine("Those who cannot remember the past are condemned to repeat it.");
+                    break;
+                
+                case "english":
+                    Console.WriteLine("Change your language and you change your thoughts");
+                    break;
+                default:
+                    Console.WriteLine("Learning is a treasure that will follow its owner everywhere");
+                    break;
+                
+                
+                
+                
+                
+                
+                
+            }
+
+            
         }
     }
 }
